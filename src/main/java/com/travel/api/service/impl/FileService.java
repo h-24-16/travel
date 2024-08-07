e83ffd2b5d6680e8f1c2191735a4a9ed4b8ed43f
ManoaLahatra <manoalahatra07@gmail.com>
2024-08-07 18:46:03 +0300

    refactor: remove UploadDir

--- src/main/java/com/travel/api/service/impl/FileService.java
+++ src/main/java/com/travel/api/service/impl/FileService.java
@@ -3,6 +3,8 @@ package com.travel.api.service.impl;
 import com.travel.api.model.FileDB;
 import com.travel.api.repository.FileRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -14,19 +16,20 @@ import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
 @Service
-@AllArgsConstructor
 public class FileService {
 
+    @Autowired
     private FileRepository fileRepository;
 
-    private UploadDir uploadDir;
+    @Value("${file.upload.dir}")
+    private String uploadDir;
 
     public FileDB saveFile(MultipartFile file) throws IOException {
         // Generate a unique file name
         String fileName = System.currentTimeMillis() + "_" + file.getOriginalFilename();
 
         // Save the file to the filesystem
-        Path copyLocation = Paths.get(uploadDir.getUploadDir() + File.separator + fileName);
+        Path copyLocation = Paths.get(uploadDir + File.separator + fileName);
         Files.copy(file.getInputStream(), copyLocation, StandardCopyOption.REPLACE_EXISTING);
 
         // Save the path and metadata to the database
