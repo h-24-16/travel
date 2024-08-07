565cb6ae82fb1c7036a82455949fe085af78d19b
ManoaLahatra <manoalahatra07@gmail.com>
2024-08-07 14:11:50 +0300

    chore: add photo attribute

--- src/main/java/com/travel/api/model/Travel.java
+++ src/main/java/com/travel/api/model/Travel.java
@@ -1,9 +1,6 @@
 package com.travel.api.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
@@ -19,5 +16,8 @@ public class Travel {
     private String destination;
     private Integer duration;
     private Double price;
+
+    @ManyToOne
+    private FileDB photo;
 
 }
