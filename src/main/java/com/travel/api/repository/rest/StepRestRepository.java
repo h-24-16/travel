72714dac2a0788be1f4dc3f8c3b0e6855bbf51c0
ManoaLahatra <manoalahatra07@gmail.com>
2024-08-07 15:23:59 +0300

    feat: getStepsByTravelId

--- src/main/java/com/travel/api/repository/rest/StepRestRepository.java
+++ src/main/java/com/travel/api/repository/rest/StepRestRepository.java
@@ -2,7 +2,12 @@ package com.travel.api.repository.rest;
 
 import com.travel.api.model.Step;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @RepositoryRestResource
-public interface StepRestRepository extends JpaRepository<Step, Long> {}
+public interface StepRestRepository extends JpaRepository<Step, Long> {
+    List<Step> findStepByTravelId(@Param("travelId") Long travelId);
+}
