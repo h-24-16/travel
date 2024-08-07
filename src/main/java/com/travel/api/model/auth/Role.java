359034e193f475f3689c8a0d1a1c17e65614fad2
ManoaLahatra <manoalahatra07@gmail.com>
2024-08-07 11:30:19 +0300

    chore: model Role

---
+++ src/main/java/com/travel/api/model/auth/Role.java
@@ -0,0 +1,26 @@
+package com.travel.api.model.auth;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role_name")
+    private Permission roleName;
+
+}
